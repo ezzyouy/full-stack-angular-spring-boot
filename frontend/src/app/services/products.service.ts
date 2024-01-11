f016e4914bc84bb11fab3261860dcf6a9ba8f2ef
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2024-01-11 18:08:12 +0100

    section 16

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,52 +1,73 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import { Observable} from "rxjs";
-import {map} from "rxjs/operators"
-import {Product} from "../common/product";
-import {ProductCategory} from "../common/product-category";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  private baseUrl="http://localhost:8080/api/products"
-  private categoryUrl="http://localhost:8080/api/product-category"
-  constructor( private http:HttpClient) { }
+  private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+  constructor(private http: HttpClient) {}
 
-  getProduct(theProductId:number):Observable<Product>{
-    const productUrl=`${this.baseUrl}/${theProductId}`
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
     return this.http.get<Product>(productUrl);
   }
-  getProductList(theCategoryId:number):Observable<Product[]>{
-    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+  getProductListPagination(
+    thePage: number,
+    thePageSize: number,
+    theCategoryId: number
+  ): Observable<GetResponseProducts> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}&page=${thePage}&size=${thePageSize}`;
+    return this.http.get<GetResponseProducts>(searchUrl);
+  }
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     return this.getProducts(searchUrl);
   }
-  getProductCatgories():Observable<ProductCategory[]>{
-    return this.http.get<GetResponseProductCategory>(this.categoryUrl).pipe(map(
-      response=>response._embedded.productCategory
-    ))
+  getProductCatgories(): Observable<ProductCategory[]> {
+    return this.http
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 
-  searchProducts(theKeyWord: string | null):Observable<Product[]> {
-    const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}`
+  searchProducts(theKeyWord: string | null): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}`;
     return this.getProducts(searchUrl);
   }
-  private getProducts(searchUrl:string):Observable<Product[]> {
+  searchProductPagination(
+    thePage: number,
+    thePageSize: number,
+    theKeyWord: string | null
+  ): Observable<GetResponseProducts> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}&page=${thePage}&size=${thePageSize}`;
+    return this.http.get<GetResponseProducts>(searchUrl);
+  }
+  private getProducts(searchUrl: string): Observable<Product[]> {
     // @ts-ignore
-    return this.http.get<GetResponseProducts>(searchUrl).pipe(map(
-      response => response._embedded.products
-    ))
+    return this.http
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
   }
 }
 
-  interface GetResponseProducts{
-  _embedded:{
-    products:Product[]
-  }
+interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
+  };
 }
-interface GetResponseProductCategory{
-  _embedded:{
-    productCategory: []
-  }
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: [];
+  };
 }
-
