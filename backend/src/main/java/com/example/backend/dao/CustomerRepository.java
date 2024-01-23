42d362e59ca407b276b4e4076608ad8e298e416d
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2024-01-23 18:01:37 +0100

    section 28

--- backend/src/main/java/com/example/backend/dao/CustomerRepository.java
+++ backend/src/main/java/com/example/backend/dao/CustomerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    Customer findByEmail(String theEmail);
 }
--- backend/src/main/java/com/example/backend/dao/ProductCategoryRepository.java
+++ backend/src/main/java/com/example/backend/dao/ProductCategoryRepository.java
@@ -3,9 +3,7 @@ package com.example.backend.dao;
 import com.example.backend.entity.ProductCatgeory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-@CrossOrigin(origins = {"http://localhost:4200","http://localhost:64737"})
 public interface ProductCategoryRepository extends JpaRepository<ProductCatgeory, Long> {
 }
