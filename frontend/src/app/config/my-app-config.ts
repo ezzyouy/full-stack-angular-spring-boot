7e23ac42b7e2c656af45f142f0da79b6dee21f15
brahimEzzyouy <b.ezzyouy@etu.enset-media.ac.ma>
2024-01-28 11:38:48 +0100

    section 32

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaeoawg03goQkZq55d7',
     issuer: 'https://dev-35138089.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
